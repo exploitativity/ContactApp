ebc61c7a08b24f0bd125bc31f97f3335a9dfe3d4
exploitativity <firerider521@gmail.com>
2017-05-11 10:15:15 -0700

    Andrew Zhang - Replaced old Android project

---
+++ ContactAppAndrewZhang/app/src/main/java/com/example/zhanga2329/contactappandrewzhang/MainActivity.java
@@ -0,0 +1,13 @@
+package com.example.zhanga2329.contactappandrewzhang;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
