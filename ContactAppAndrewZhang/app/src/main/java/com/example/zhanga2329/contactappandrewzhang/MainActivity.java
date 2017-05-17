53c313e74b4344c9772b6850de149c45c62992f0
exploitativity <firerider521@gmail.com>
2017-05-17 10:55:50 -0700

    Andrew Zhang - implemented data display and search
    Data displays in a dialogue alert box but is not formatted very well.
    Search by name works for any possible substring of the name value,
    displays in same alert box but only the rows that match the search text.
    Will implement case insensitivity later.

--- ContactAppAndrewZhang/app/src/main/java/com/example/zhanga2329/contactappandrewzhang/MainActivity.java
+++ ContactAppAndrewZhang/app/src/main/java/com/example/zhanga2329/contactappandrewzhang/MainActivity.java
@@ -1,6 +1,7 @@
 package com.example.zhanga2329.contactappandrewzhang;
 
 import android.database.Cursor;
+import android.support.v7.app.AlertDialog;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
@@ -14,6 +15,7 @@ public class MainActivity extends AppCompatActivity {
     EditText editName;
     EditText editAge;
     EditText editAddress;
+    EditText editSearch;
 
     Button btnAddData;
 
@@ -27,6 +29,7 @@ public class MainActivity extends AppCompatActivity {
         editName = (EditText)(findViewById(R.id.editText_name));
         editAge = (EditText)(findViewById(R.id.editText_age));
         editAddress = (EditText)(findViewById(R.id.editText_address));
+        editSearch = (EditText) (findViewById(R.id.editText_search));
     }
     public void addData(View v) {
         boolean isInserted = myDb.insertData(editName.getText().toString(), editAge.getText().toString(), editAddress.getText().toString());
@@ -46,19 +49,73 @@ public class MainActivity extends AppCompatActivity {
         Cursor res = myDb.getAllData();
         if(res.getCount() == 0){
             Log.d("MyContact", "Database is empty!");
-            showMessage("Error", "No data found in database");
+            showMessage("Error", "No data found in database, res.getCount = " +res.getCount());
             //output message using Log.d and Toast
             return;
         }
 
         StringBuffer buffer = new StringBuffer();
         //setup a loop with Cursor (res) using moveToNext
+        for(int i = 0; i < res.getColumnCount(); i++)
+        {
+            buffer.append(res.getColumnName(i));
+            buffer.append("\t");
+        }
+        buffer.append("\n");
+        res.moveToFirst();
+        do
+        {
+            for(int i = 0; i < res.getColumnCount(); i++)
+            {
+                buffer.append(res.getString(i));
+                buffer.append("\t");
+            }
+            buffer.append("\n");
+        }while(res.moveToNext());
         // append each COL to the buffer
         // display message using showMessage
         showMessage("Data", buffer.toString());
 
     }
+    public void search(View v) {
+        Cursor res = myDb.getAllData();
+        if(res.getCount() == 0){
+            Log.d("MyContact", "Database is empty!");
+            showMessage("Error", "No data found in database, res.getCount = " +res.getCount());
+            //output message using Log.d and Toast
+            return;
+        }
+
+        StringBuffer buffer = new StringBuffer();
+        //setup a loop with Cursor (res) using moveToNext
+        for(int i = 0; i < res.getColumnCount(); i++)
+        {
+            buffer.append(res.getColumnName(i));
+            buffer.append("\t");
+        }
+        buffer.append("\n");
+        res.moveToFirst();
+        do
+        {
+            if(res.getString(res.getColumnIndex("NAME")).indexOf(editSearch.getText().toString()) > -1){
+                for(int i = 0; i < res.getColumnCount(); i++)
+                {
+                    buffer.append(res.getString(i));
+                    buffer.append("\t");
+                }
+                buffer.append("\n");
+            }
+        }while(res.moveToNext());
+        // append each COL to the buffer
+        // display message using showMessage
+        showMessage("Search Results", buffer.toString());
+    }
+
     private void showMessage(String title, String message) {
-    //AlertDialg.Builder
+        AlertDialog.Builder builder = new AlertDialog.Builder(this);
+        builder.setCancelable(true);
+        builder.setTitle(title);
+        builder.setMessage(message);
+        builder.show();
     }
 }
